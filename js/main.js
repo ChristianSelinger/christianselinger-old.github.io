c7cc1f9d681e6c3d2cb9ea6c352641ed40039d85
ChristianSelinger <94453136+ChristianSelinger@users.noreply.github.com>
2022-09-19 10:11:33 +0200

    ross article

--- js/main.js
+++ js/main.js
@@ -315,6 +315,8 @@ Maynard Smith J, Price GR:${OUTPUT_STRING}${glow("The Logic of Animal Conflict")
 
 Price GR:${OUTPUT_STRING}${glow("Selection and Covariance")} Nature 1970${OUTPUT_STRING}http://www0.cs.ucl.ac.uk/staff/W.Langdon/ftp/papers/price_nature.pdf
 
+Ross R:${OUTPUT_STRING}${glow("An application of the theory of probabilities to the study of a priori pathometry.")}  Proceedings of the Royal Society of London 1916${OUTPUT_STRING}https://royalsocietypublishing.org/doi/pdf/10.1098/rspa.1916.0007?download=true
+
 `,
   credits: `${banner}
 
